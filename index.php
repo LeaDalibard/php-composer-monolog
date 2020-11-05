ae4d24f4d10750c6107a913796d1117507c39e64
Lea Dalibard <leadalibard@gmail.com>
2020-11-05 15:43:36 +0100

    Adding last updates emergency + alert

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ use \Monolog\Handler\NativeMailerHandler;
 $loggerInfo = new Logger('info');
 $loggerWarning = new Logger('warning');
 $loggerDanger = new Logger('danger');
+$loggerEmergency=new Logger('emergency');
 
 // Now add some handlers
 $loggerInfo->pushHandler(new FirePHPHandler());
@@ -29,6 +30,9 @@ $loggerWarning->pushHandler(new BrowserConsoleHandler());
 $loggerDanger->pushHandler(new FirePHPHandler());
 $loggerDanger->pushHandler(new BrowserConsoleHandler());
 
+$loggerEmergency->pushHandler(new StreamHandler(__DIR__ . '/emergency.log', Logger::EMERGENCY));
+$loggerEmergency->pushHandler(new FirePHPHandler());
+$loggerEmergency->pushHandler(new BrowserConsoleHandler());
 
 if(isset($_GET['type'])){
     switch ($_GET['type']) {
@@ -59,12 +63,21 @@ if(isset($_GET['type'])){
         case "CRITICAL":
             $loggerDanger->pushHandler(new StreamHandler(__DIR__ . '/danger.log', Logger::CRITICAL));
             $loggerDanger->critical($_GET['message']);
+            $loggerDanger->pushHandler(new NativeMailerHandler(  'leadalibard@gmail.com','critical','leadalibard@gmail.com'));
             break;
 
         case "ALERT":
             $loggerDanger->pushHandler(new StreamHandler(__DIR__ . '/danger.log', Logger::ALERT));
             $loggerDanger->alert($_GET['message']);
+            $loggerDanger->pushHandler(new NativeMailerHandler(  'leadalibard@gmail.com','alert','leadalibard@gmail.com'));
             break;
+
+        case "EMERGENCY":
+            $loggerEmergency->pushHandler(new StreamHandler(__DIR__ . '/danger.log', Logger::ALERT));
+            $loggerEmergency->emergency($_GET['message']);
+            $loggerDanger->pushHandler(new NativeMailerHandler(  'leadalibard@gmail.com','emergency','leadalibard@gmail.com'));
+            break;
+
     }
 
 
